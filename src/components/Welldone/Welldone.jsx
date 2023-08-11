7e285b418545df141dfc232e46fc17313edbbab2
Aslanov Rustam <aslanovrg@gmail.com>
2023-08-11 12:34:36 +0300

    start developing desctop view, add readme

--- src/components/Welldone/Welldone.jsx
+++ src/components/Welldone/Welldone.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import s from "./welldone.module.css";
+import girl from "../../images/girl.png";
 
 export default function Welldone() {
-  return <div>Welldone</div>;
+  return (
+    <div className={s.container}>
+      <img className={s.girl} src={girl} alt="girl AI" />
+      <div className={s.box}>
+        <p className={s.titleGreat}>
+          Отлично!<br></br> Остался последний шаг:
+        </p>
+        <p className={s.count}>
+          пополните ваш счет<br></br> чтобы начать игру
+        </p>
+        <p className={s.economy}>100% экономии! </p>
+        <p className={s.bonus}>
+          50$ депозит + <br></br>50$ бонус
+        </p>
+        <button
+          type="button"
+          className={s.btn}
+          onClick={() => console.log("click")}
+        >
+          Пополнить счет
+        </button>
+      </div>
+    </div>
+  );
 }
