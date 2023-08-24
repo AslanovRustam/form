9af5f3f3289dcf2715cd55109b59332a2938591f
Aslanov Rustam <aslanovrg@gmail.com>
2023-08-24 14:48:17 +0300

    upd imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState, Suspense } from "react";
+import { useTranslation, Trans } from "react-i18next";
 import Container from "./components/Container/Container";
 import Main from "./components/Main/Main";
 import Welldone from "./components/Welldone/Welldone";
 import Girl from "./components/Girl/Girl";
-import { useTranslation, Trans } from "react-i18next";
 
 function App() {
   const [step, setStep] = useState(1);
